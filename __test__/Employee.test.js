e256275f8b0315dd949a32b95d8f6a652fedff9b
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-04-19 18:20:43 -0500

    generate new test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -8,3 +8,33 @@ test("creates an Employee object", () => {
   expect(employee.id).toBe("1");
   expect(employee.email).toBe("johnsmith@gmail.com");
 });
+
+test("get employee's name value", () => {
+  const employee = new Employee("John Smith", "1", "johnsmith@gmail.com");
+
+  expect(employee.getName()).toEqual(
+    expect.stringContaining(employee.name.toString())
+  );
+});
+
+test("get employee's id value", () => {
+  const employee = new Employee("John Smith", "1", "johnsmith@gmail.com");
+
+  expect(employee.getId()).toEqual(
+    expect.stringContaining(employee.id.toString())
+  );
+});
+
+test("get employee's email value", () => {
+  const employee = new Employee("John Smith", "1", "johnsmith@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+test("get employee's role value", () => {
+  const employee = new Employee("John Smith", "1", "johnsmith@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
+});
