788613c48c80c4acd161f7e6363e9fe5a0cce201
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-04-19 17:24:05 -0500

    adding clases

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,11 @@
 const Engineer = require("../lib/Engineer.js");
+
+test("creates an Engineer object", () => {
+  const engineer = new Engineer("John Smith", "1", "johnsmith@gmail.com");
+
+  expect(employee.name).toBe("John Smith");
+  expect(employee.id).toBe("1");
+  expect(employee.email).toBe("johnsmith@gmail.com");
+
+  console.log(employee);
+});
